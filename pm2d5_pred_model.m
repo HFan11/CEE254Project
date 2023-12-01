25b43f4bdaaa5db27c6515a7171dc6a11f3f2b3f
HFan11 <hfan11@stanford.edu>
2023-11-30 21:55:31 -0800

    Update pm2d5_pred_model.m

--- pm2d5_pred_model.m
+++ pm2d5_pred_model.m
@@ -1,10 +1,9 @@
 function pred_pm2d5 = pm2d5_pred_model(train_data, test_data, problem_type)
     if problem_type == 1
-        %pred_pm2d5 = 1;
         pred_pm2d5 = grp_model(train_data, test_data, 1);
     elseif problem_type == 2
-        pred_pm2d5 = Michael_pred_model(train_data, test_data);
+        pred_pm2d5 = Michael_pred_model(train_data, test_data, 2);
     elseif problem_type == 3
-        pred_pm2d5 = grp_model(train_data, test_data, 3);
+        pred_pm2d5 = Michael_pred_model(train_data, test_data, 3);
     end
 end
